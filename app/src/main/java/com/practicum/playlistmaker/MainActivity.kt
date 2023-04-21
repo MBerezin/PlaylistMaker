9f912436d58aac3350d0bba00b8a7e78297ba4d2
Maxim Berezin <beresin.m@googlemail.com>
2023-04-21 21:06:12 +0300

    add onclick listeners for buttons

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,34 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.search_btn)
+        val btnMedia = findViewById<Button>(R.id.media_btn)
+        val btnSettings = findViewById<Button>(R.id.settings_btn)
+
+        btnSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnMediaClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnMedia.setOnClickListener(btnMediaClickListener)
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
