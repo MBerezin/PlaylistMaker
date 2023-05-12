f6ca3ae27467e40843bd6f271082ac91a0e5694b
Maxim Berezin <beresin.m@googlemail.com>
2023-05-12 19:21:11 +0300

    add tracks search handling

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,16 +1,21 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
@@ -18,10 +23,25 @@ class SearchActivity : AppCompatActivity() {
         var searchText: String = String()
     }
 
+    private val itunesBaseUrl = "http://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val itunesService = retrofit.create(ITunesApi::class.java)
 
     private lateinit var inpEditText: EditText
     private lateinit var imgClear: ImageView
     private lateinit var recycler: RecyclerView
+    private lateinit var linearLayoutPlaceholder: LinearLayout
+    private lateinit var imgPlaceholder: ImageView
+    private lateinit var txtPlaceholder: TextView
+    private lateinit var btnReload: Button
+
+    private val tracks = arrayListOf<Track>()
+    private val adapter = TrackAdapter(tracks)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,38 +50,26 @@ class SearchActivity : AppCompatActivity() {
         imgClear = findViewById<ImageView>(R.id.img_clear)
         inpEditText = findViewById<EditText>(R.id.et_search)
         recycler = findViewById<RecyclerView>(R.id.trackList)
-        val tracks = arrayListOf<Track>(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
+        linearLayoutPlaceholder = findViewById<LinearLayout>(R.id.linearLayoutPlaceholder)
+        imgPlaceholder = findViewById<ImageView>(R.id.imageViewPlaceholder)
+        txtPlaceholder = findViewById<TextView>(R.id.textViewPlaceholder)
+        btnReload = findViewById<Button>(R.id.buttonReload)
+
+
 
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = TrackAdapter(
-            tracks = tracks
-        )
+        recycler.adapter = adapter
+
+        btnReload.setOnClickListener{
+            searchTracks()
+        }
 
         imgClear.setOnClickListener {
             inpEditText.setText("")
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(inpEditText.windowToken, 0)
+            tracks.clear()
+            adapter.notifyDataSetChanged()
         }
 
         val textWatcher = object : TextWatcher {
@@ -79,6 +87,58 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inpEditText.addTextChangedListener(textWatcher)
+
+        inpEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if(inpEditText.text.isNotEmpty()){
+                    searchTracks()
+                }
+                true
+            }
+            false
+        }
+    }
+
+    private fun searchTracks()  {
+        itunesService.search(inpEditText.text.toString()).enqueue(object : Callback<TracksResponse>{
+            override fun onResponse(
+                call: Call<TracksResponse>,
+                response: Response<TracksResponse>
+            ) {
+                if (response.code() == 200){
+                    tracks.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        tracks.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                    }
+                    if (tracks.isEmpty()) {
+                        linearLayoutPlaceholder.visibility = View.VISIBLE
+                        txtPlaceholder.text = getString(R.string.no_results)
+                        imgPlaceholder.setImageResource(R.drawable.noresults)
+                    } else {
+                        linearLayoutPlaceholder.visibility = View.GONE
+                    }
+                } else {
+                    tracks.clear()
+                    linearLayoutPlaceholder.visibility = View.VISIBLE
+                    btnReload.visibility = View.VISIBLE
+                    txtPlaceholder.text = getString(R.string.connection_problem)
+                        .plus("\n")
+                        .plus(getString(R.string.loading_fail))
+                    imgPlaceholder.setImageResource(R.drawable.connectionproblem)
+                }
+            }
+
+            override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                tracks.clear()
+                linearLayoutPlaceholder.visibility = View.VISIBLE
+                btnReload.visibility = View.VISIBLE
+                txtPlaceholder.text = getString(R.string.connection_problem)
+                    .plus("\n")
+                    .plus(getString(R.string.loading_fail))
+                imgPlaceholder.setImageResource(R.drawable.connectionproblem)
+            }
+        })
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
