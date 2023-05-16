3641a85a0cc75669fb8f088db170ddb2cf97fe6a
Maxim Berezin <beresin.m@googlemail.com>
2023-05-16 23:05:00 +0300

    move inflate to adapter, add onclick listener for viewholder view

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -1,17 +1,26 @@
 package com.practicum.playlistmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(
-    private val tracks: ArrayList<Track>
-) : RecyclerView.Adapter<TrackViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder = TrackViewHolder(parent)
+    private val tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    var itemClickListener: ((Int, Track) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder{
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHolder(view)
+    }
 
     override fun getItemCount() = tracks.size
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener() {
+            itemClickListener?.invoke(position, tracks[position])
+        }
     }
 
 }
