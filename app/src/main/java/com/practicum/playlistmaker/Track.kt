98dd977602f8405e54178a29c427a1ef3e49e6da
Maxim Berezin <beresin.m@googlemail.com>
2023-05-16 22:56:32 +0300

    add new property - trackId

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -4,5 +4,6 @@ data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val artworkUrl100: String, // Ссылка на изображение обложки
-    val trackTimeMillis: String // Продолжительность трека
+    val trackTimeMillis: String, // Продолжительность трека
+    val trackId: Int // уникальный идентификатор трека
 )
