9fcabff4f328d27e3e32ae6fbfb1a87a3859ef4c
Maxim Berezin <beresin.m@googlemail.com>
2023-05-16 23:02:00 +0300

    add theme switch handling

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -7,14 +7,25 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var imgShare: ImageView
+    private lateinit var imgSupport: ImageView
+    private lateinit var imgTermsOfUse: ImageView
+    private lateinit var switchTheme: com.google.android.material.switchmaterial.SwitchMaterial
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val toolbar  = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        val imgShare = findViewById<ImageView>(R.id.img_share)
-        val imgSupport = findViewById<ImageView>(R.id.img_support)
-        val imgTermsOfUse = findViewById<ImageView>(R.id.img_terms_of_use)
+        toolbar  = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        imgShare = findViewById<ImageView>(R.id.img_share)
+        imgSupport = findViewById<ImageView>(R.id.img_support)
+        imgTermsOfUse = findViewById<ImageView>(R.id.img_terms_of_use)
+        switchTheme = findViewById<com.google.android.material.switchmaterial.SwitchMaterial>(R.id.switch_theme)
+
+        val sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        switchTheme.isChecked = sharedPrefs.getBoolean(THEME_SWITCHER_KEY, false)
 
         toolbar.setNavigationOnClickListener {
             finish()
@@ -42,6 +53,16 @@ class SettingsActivity : AppCompatActivity() {
             val link: Uri = Uri.parse("https://yandex.ru/legal/practicum_offer/")
             val linkIntent = Intent(Intent.ACTION_VIEW, link)
             startActivity(linkIntent)
+        }
+
+
+        switchTheme.setOnCheckedChangeListener { switcher, checked ->
+            (applicationContext as App).switchTheme(checked)
+
+            val sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+            sharedPrefs.edit()
+                .putBoolean(THEME_SWITCHER_KEY, checked)
+                .apply()
         }
 
     }
