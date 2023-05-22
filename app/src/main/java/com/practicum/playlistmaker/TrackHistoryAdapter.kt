a99febb0a441476da83bea2ec24893b3bcaec69a
Maxim Berezin <beresin.m@googlemail.com>
2023-05-22 13:04:54 +0300

    add on click listener

--- app/src/main/java/com/practicum/playlistmaker/TrackHistoryAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackHistoryAdapter.kt
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 class TrackHistoryAdapter(
     private val tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
 
+    var itemClickListener: ((Track) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder{
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
         return TrackViewHolder(view)
@@ -16,6 +18,9 @@ class TrackHistoryAdapter(
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener() {
+            itemClickListener?.invoke(tracks[position])
+        }
     }
 
 }
