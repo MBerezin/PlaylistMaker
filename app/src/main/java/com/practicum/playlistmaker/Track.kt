3be7e8a95d9cb3e86a5fbecc6560d811ce22fce6
Maxim Berezin <beresin.m@googlemail.com>
2023-05-22 13:04:01 +0300

    extend track with new data

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -5,5 +5,23 @@ data class Track(
     val artistName: String, // Имя исполнителя
     val artworkUrl100: String, // Ссылка на изображение обложки
     val trackTimeMillis: String, // Продолжительность трека
-    val trackId: Int // уникальный идентификатор трека
-)
+    val trackId: Int, // уникальный идентификатор трека,
+    val collectionName: String, // Название альбома
+    @get:JvmName("track_releaseDate")
+    val releaseDate: String,// Год релиза трека
+    val primaryGenreName: String,// Жанр трека
+    val country: String)// Страна исполнителя
+{
+    fun getCoverArtwork(): String{
+        return artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+    }
+
+    fun getReleaseDate(): String{
+        return if(releaseDate.isNotEmpty()){
+            releaseDate.substring(0,4)
+        } else {
+            ""
+        }
+    }
+
+}
