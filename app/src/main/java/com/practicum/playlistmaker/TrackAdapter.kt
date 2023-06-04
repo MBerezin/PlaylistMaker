ea6c46a4c1f91cdf867f54f84d696aaec9170659
Maxim Berezin <beresin.m@googlemail.com>
2023-06-04 11:56:51 +0300

    move click listener to constructor

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -5,9 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(
-    private val tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
-
-    var itemClickListener: ((Track) -> Unit)? = null
+    private val tracks: ArrayList<Track>, private val itemClickListener: ((Track) -> Unit)) : RecyclerView.Adapter<TrackViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder{
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
@@ -19,7 +17,7 @@ class TrackAdapter(
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
         holder.itemView.setOnClickListener() {
-            itemClickListener?.invoke(tracks[position])
+            itemClickListener.invoke(tracks[position])
         }
     }
 
