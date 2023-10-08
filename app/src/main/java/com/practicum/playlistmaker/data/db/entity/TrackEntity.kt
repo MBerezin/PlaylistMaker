5c21b3eb0ad58080fc80dc3c47cc4f257d66ffcb
Maxim Berezin <beresin.m@googlemail.com>
2023-10-08 19:34:55 +0300

    create entity for track

---
+++ app/src/main/java/com/practicum/playlistmaker/data/db/entity/TrackEntity.kt
@@ -0,0 +1,19 @@
+package com.practicum.playlistmaker.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "track_table")
+data class TrackEntity(
+    @PrimaryKey
+    val id: Int,
+    val artworkUrl100: String,
+    val trackName: String,
+    val artistName: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String,
+    val trackTimeMillis: String,
+    val previewUrl: String?
+)
